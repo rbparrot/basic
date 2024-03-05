806ba4910ce544b4ee23fb7036ad3ce0a73523c4
Parrot <hy04240@gmail.com>
2024-03-05 18:53:23 +0900

    출력 / 스캔

---
+++ Example.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Example {
+    public static void main(String[] args){
+
+        System.out.println("Hello, world - Java");
+        Scanner sc = new Scanner(System.in);
+        sc.next();
+    }
+}
